3c14db23ebc763fccd45a473ab02117318c45913
C:\Users\Robik\Desktop\Проекты\React\solmlink <schedamian@gmail.com>
2023-04-14 19:33:39 +0300

    add fix strategy

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,6 @@
-new Swiper('.swiper-block', {
+const currentSlideWidth = window.innerWidth;
+
+new Swiper('.slider__block', {
   slidesPerView: 1,
   spaceBetween: 15,
   loop: true,
@@ -7,3 +9,16 @@ new Swiper('.swiper-block', {
     // disableOnInteraction: false,
   },
 });
+if (currentSlideWidth > 992) {
+  new Swiper('.strategy__right', {
+    slidesPerView: 1,
+    spaceBetween: 15,
+    speed: 800,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      // disableOnInteraction: false,
+    },
+    // direction: 'vertical',
+  });
+}
