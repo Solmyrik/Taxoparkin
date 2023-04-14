3b3d51fca07a90a643b9d7dea787f71216a35b0d
C:\Users\Robik\Desktop\Проекты\React\solmlink <schedamian@gmail.com>
2023-04-14 18:31:18 +0300

    fix slider and add social link

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,9 @@
 new Swiper('.swiper-block', {
   slidesPerView: 1,
   spaceBetween: 15,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    // disableOnInteraction: false,
+  },
 });
---
+++ js/strategy.js
@@ -0,0 +1,17 @@
+// const strategy = document.querySelector('.strategy');
+// const valueStrategy = strategy.getBoundingClientRect().y;
+
+// console.log(valueStrategy);
+
+// document.addEventListener('scroll', (e) => {
+//   if (strategy.getBoundingClientRect().y < 0) {
+//    let currentPosition = window.pageYOffset;
+
+//    // Затем останавливаем скролл на 1 секунду
+//    window.scroll(0, currentPosition);
+//    setTimeout(function() {
+//      // Возвращаем позицию обратно после 1 секунды
+//      window.scroll(0, currentPosition);
+//    }, 1000); // 1000 миллисекунд = 1 секунда
+//   }
+// });
